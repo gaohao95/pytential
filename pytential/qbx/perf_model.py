a796934e34d5565faeae3a99c7b3ac1040746374
Hao Gao <gaohao95@gmail.com>
2018-08-02 16:22:20 -0500

    Add p2qbxl to counter

--- pytential/qbx/perf_model.py
+++ pytential/qbx/perf_model.py
@@ -1,6 +1,7 @@
 import numpy as np
-from boxtree.distributed.perf_model import PerformanceCounter
+from boxtree.distributed.perf_model import PerformanceCounter, PerformanceModel
 from collections import namedtuple
+import pyopencl as cl
 
 QBXParameters = namedtuple(
     "QBXParameters",
@@ -99,3 +100,89 @@ class QBXPerformanceCounter(PerformanceCounter):
                 direct_workload[itgt_box] = count
 
         return direct_workload
+
+    def count_p2qbxl(self, use_global_idx=False):
+        geo_data = self.geo_data
+        traversal = self.traversal
+        tree = traversal.tree
+
+        if use_global_idx:
+            np2qbxl = np.zeros((tree.nboxes,), dtype=np.intp)
+        else:
+            ntarget_boxes = len(traversal.target_boxes)
+            np2qbxl = np.zeros((ntarget_boxes,), dtype=np.intp)
+
+        for tgt_icenter in geo_data.global_qbx_centers:
+            itgt_box = geo_data.qbx_center_to_target_box[tgt_icenter]
+
+            np2qbxl_srcs = 0
+
+            # list 1
+            start, end = traversal.neighbor_source_boxes_starts[
+                            itgt_box:itgt_box + 2]
+            for src_ibox in traversal.neighbor_source_boxes_lists[start:end]:
+                np2qbxl_srcs += tree.box_source_counts_nonchild[src_ibox]
+
+            # list 3 close
+            if traversal.from_sep_close_smaller_starts is not None:
+                start, end = traversal.from_sep_close_smaller_starts[
+                                itgt_box:itgt_box + 2]
+                for src_ibox in traversal.from_sep_close_smaller_lists[start:end]:
+                    np2qbxl_srcs += tree.box_source_counts_nonchild[src_ibox]
+
+            # list 4 close
+            if traversal.from_sep_close_bigger_starts is not None:
+                start, end = traversal.from_sep_close_bigger_starts[
+                                itgt_box:itgt_box + 2]
+                for src_ibox in traversal.from_sep_close_bigger_lists[start:end]:
+                    np2qbxl_srcs += tree.box_source_counts_nonchild[src_ibox]
+
+            workload = np2qbxl_srcs * self.parameters.ncoeffs_qbx
+
+            if use_global_idx:
+                np2qbxl[traversal.target_boxes[itgt_box]] += workload
+            else:
+                np2qbxl[itgt_box] += workload
+
+        return np2qbxl
+
+
+class QBXPerformanceModel(PerformanceModel):
+
+    def time_performance(self, geo_data):
+        traversal = geo_data.traversal()
+
+        wrangler = self.wrangler_factory(tree=traversal.tree)
+
+        counter = QBXPerformanceCounter(
+            geo_data, wrangler, self.uses_pde_expansions
+        )
+
+        # Record useful metadata for assembling performance data
+        nm2p, nm2p_boxes = counter.count_m2p()
+
+        timing_data = {
+            "nterms_fmm_total": counter.count_nters_fmm_total(),
+            "direct_workload": np.sum(counter.count_direct()),
+            "direct_nsource_boxes": traversal.neighbor_source_boxes_starts[-1],
+            "m2l_workload": np.sum(counter.count_m2l()),
+            "m2p_workload": np.sum(nm2p),
+            "m2p_nboxes": np.sum(nm2p_boxes),
+            "p2l_workload": np.sum(counter.count_p2l()),
+            "p2l_nboxes": np.sum(counter.count_p2l_source_boxes()),
+            "eval_part_workload": np.sum(counter.count_eval_part()),
+            "p2qbxl_workload": np.sum(counter.count_p2qbxl())
+        }
+
+        # Generate random source weights
+        with cl.CommandQueue(self.cl_context) as queue:
+            source_weights = self.rng.uniform(
+                queue,
+                traversal.tree.nsources,
+                traversal.tree.coord_dtype
+            ).get()
+
+        # Time a FMM run
+        self.drive_fmm(traversal, wrangler, source_weights, timing_data=timing_data)
+
+        self.time_result.append(timing_data)
